3ab761e20705407952b110cfe574422c534fd817
Javier Leon <mrm.leon93@gmail.com>
2024-10-16 01:57:23 -0400

    navbar btn animation v1 update

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,24 +3,25 @@ import logo from '../../assets/homePageLogo.png'
 import Logo from '../Logo/Logo'
 const Nav = () => {
   return (
-    <header className={styles.navbar}>
+    <div className={styles.navbar}>
          <div className={styles.navbarLogo}>
           <a href="#home" style={{
             color: 'white'
           }}><Logo/></a>
         </div>
-      <nav className={styles.navbarMenu}>
-       
+      <div className={styles.navbarMenu}>
+       <div className={styles.brackets}>
         <ul>
-          <li><a href="#home">Home</a></li>
+          <li ><a href="#home">Home</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#products">Products</a></li>
           <li><a href="#about">About</a></li>
           <button className="btn btn-primary px-8"> <a href="#signup">SIGN UP</a></button>
         </ul>
-      </nav>
+        </div>
+      </div>
      
-    </header>
+    </div>
   )
 }
 
