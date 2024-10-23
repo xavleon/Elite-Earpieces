845e460a1bd58ecce9cbeae29823110c9913cd8a
Javier Leon <mrm.leon93@gmail.com>
2024-10-23 01:24:53 -0400

    feature icons

--- src/components/Features/FeatureData.ts
+++ src/components/Features/FeatureData.ts
@@ -1,37 +1,37 @@
 const featureData = [
     {
         id: 1,
-        icon:"fi fi-brands-apple",
+        icon:"fa-regular fa-thumbs-up",
         Title: "Interactive Product Customization",
         Description: "Allow users to customize their earbuds by choosing colors, case designs, or engraving options. Display a live 3D model of the earbuds that updates with their selections."
     },
     {
         id: 2,
-        icon: 'fa-regular fa-thumbs-up',
+        icon: "fa-solid fa-shield-halved",
         Title: "Sound Quality Comparison Tool",
         Description: " Offer a feature where users can compare the sound quality of different models. This could include demos of various sound profiles (e.g., bass-heavy, balanced, or treble-focused) to help them pick the best fit for their preferences."
     },
     {
         id: 3,
-        icon: '<i class="fi fi-rr-social-network"></i>',
+        icon: "fa-solid fa-truck",
         Title: "Personalized Fit Guide",
         Description: "Provide an interactive guide where customers can answer a few questions or use AI-powered sizing recommendations to find the best earbud size and style for their ears. This would enhance comfort and satisfaction."
     },
     {
         id: 4,
-        icon: '<i class="fi fi-rr-social-network"></i>',
+        icon: "fa-solid fa-cloud-arrow-up",
         Title: "Noise Cancellation Preview",
         Description: "Offer an interactive feature where users can experience a simulated preview of the earbuds' noise cancellation capabilities. They could toggle between different ambient sounds (e.g., city noise, office chatter) to see how effectively the earbuds block out background noise."
     },
     {
         id: 5,
-        icon: "fi fi-brands-apple",
+        icon: "fa-solid fa-location-dot",
         Title: "User Reviews with Audio Uploads",
         Description: "Allow customers to leave reviews with the option to upload short audio clips or recordings. This feature would let potential buyers hear real-world examples of sound quality in different environments and help build trust in the product."
     },
     {
         id: 6,
-        icon: '<i class="fi fi-rr-social-network"></i>',
+        icon: "fa-solid fa-heart-pulse",
         Title: "Live Chat Support with Product Experts",
         Description: "Include a live chat or chatbot feature where users can instantly connect with experts who can guide them through choosing the right earbuds, troubleshooting, or providing product recommendations."
     }
